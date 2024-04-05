c7443ab09229000e65ed5cf51792c5e8d09bb5a7
Karthik10010 <karthikpemmadi619@gmail.com>
2024-04-05 22:47:30 +0530

    sab

--- editor/editor/urls.py
+++ editor/editor/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for editor project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,5 +5,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
     path('student/', include('student.urls')),
+<<<<<<< HEAD
     
+=======
+    path('teacher/', include('teacher.urls')),
+>>>>>>> 2a8ee6e985f0a54c575c240b9f2c97ad16f10442
 ]
