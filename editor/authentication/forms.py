b08d89021d99a1460a4bea8f25ee7435a6aa021c
Ishan Singla <singlaishan69@gmail.com>
2024-04-05 19:19:16 +0530

    Boht kuch dala

---
+++ editor/authentication/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+class SignUpForm(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'abc@mail.com'}))
+    rollnumber = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Roll Number'}))
+    pass1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    pass2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        pass1 = cleaned_data.get("pass1")
+        pass2 = cleaned_data.get("pass2")
+
+        if pass1 != pass2:
+            raise forms.ValidationError("Passwords do not match.")
+        
+class LoginForm(forms.Form):
+    rollnumber = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Roll Number'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
--- editor/student/views.py
+++ editor/student/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     
     return render(request, 'student/start_test.html')
+
+def test(request):
+    return render(request, 'student/testAppear.html')
