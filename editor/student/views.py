84a57f16eddd713e2036069653ba821828e537bb
Ishan Singla <singlaishan69@gmail.com>
2024-04-07 03:35:05 +0530

    sync

--- editor/student/views.py
+++ editor/student/views.py
@@ -7,16 +7,25 @@ from authentication.models import CustomUser
 
 def index(request):
     if request.method == 'POST':
-        examcode_ = request.POST.get("code")
-        matchingtest = Test.objects.filter(subjectCode = examcode_)
-        return render(request, 'student/testAppear.html')
+        examcode = request.POST.get("code")
+
+        # Check if the exam code exists
+        matching_tests = Test.objects.filter(exam_code=examcode)
+        if matching_tests.exists():
+            # The exam code exists
+            return render(request, 'student/testAppear.html')
+        else:
+            # The exam code does not exist
+            form = ExamCodeInput()
+            user = request.session.get('name','guest')
+            return render(request, 'student/start_test.html', {'form': form, "name": user, "error": "Invalid exam code."})
     else:
         form = ExamCodeInput()
         user = request.session.get('name','guest')
-    return render(request, 'student/start_test.html', {'form': form,"name":user})
+        return render(request, 'student/start_test.html', {'form': form, "name": user})
 
 
 def test(request):
-    return render(request, 'student/testAppear.html')
+    return render(request, 'student/testAppear2.html')
 
 
